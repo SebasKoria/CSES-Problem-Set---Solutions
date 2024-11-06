605a7cdeb56dcee3f54dfed86ca1a5cf1f450d16
Zirok15 <sebastiankoriacasado@gmail.com>
2024-11-06 08:39:32 -0400

    Added Maximum Subarray Sum solution

--- Maximum Subarray Sum/main.cpp
+++ Maximum Subarray Sum/main.cpp
@@ -5,16 +5,18 @@ using namespace std;
 int main()
 {
     int n; cin >> n;
+    long long int mx = INT_MIN;
     long long int sum = 0;
     long long int ans = 0;
 
     while(n--){
         long long int in; cin >> in;
+        mx = max(mx, in);
         sum = max(0ll, sum + in);
         ans = max(ans, sum);
     }
 
-    cout << ans << endl;
+    cout << (ans == 0 ? mx : ans) << endl;
 
     return 0;
 }
